181930b7b8cc65785d59bc87c6a47a2b751e365c
bigstar7966 <daesung09854@gmail.com>
2020-06-12 12:06:48 +0900

    [Add]
    home screen

--- moview/src/pages/Home.js
+++ moview/src/pages/Home.js
@@ -1,11 +1,48 @@
 import React from 'react';
+import styled from 'styled-components';
+import Header from '../components/Header';
+import Slider from 'react-slick';
+import '../styles/Home.css';
+
+
+const Container = styled.div`
+    display : flex;
+    width : 100vw;
+    height : 100vh;
+    flex-direction : column;
+    align-items : center;
+`;
+
+const Body = styled.div`
+    display : flex;
+    width : 80%;
+    height : 100%;
+    flex-direction : column;
+    background-color : blue;
+`;
+
+const SlideContainer = styled.div`
+    display : flex;
+    width : 100%;
+    height : 40%;
+    background-color : red;
+`;
 
 class Home extends React.Component{
+    refreshPage(){
+        window.location.reload(false);
+    }
+
     render(){
         return(
-            <div>
-                <h1>Home</h1>
-            </div>
+            <Container className = "Container">
+                <Header />
+                <Body>
+                    <SlideContainer>
+                        
+                    </SlideContainer>
+                </Body>
+            </Container>
         );
     }
 };
