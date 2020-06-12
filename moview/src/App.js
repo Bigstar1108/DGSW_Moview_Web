081ee4e6826b2ad722790787daf2ba6f032a1c53
bigstar7966 <daesung09854@gmail.com>
2020-06-12 12:06:04 +0900

    [Add]
    add route home, login, join, profile

--- moview/src/App.js
+++ moview/src/App.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import styled, { createGlobalStyle } from 'styled-components';
+
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Join from './pages/Join';
+import Profile from './pages/Profile';
+
+const Rootdiv = styled.div`
+  height : 100vh;
+`;
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin : 0;
+    padding : 0;
+  }
+`;
 
 class App extends React.Component{
   render(){
     return(
-      <div>
-        <Route exact path = "/" component = {Home} />
-      </div>
+      <React.Fragment>
+        <GlobalStyle />
+        <Rootdiv>
+          <Route exact path = "/" component = {Home} />
+          <Route path = "/login" component = {Login} />
+          <Route path = "/join" component = {Join} />
+          <Route path = "/profile" component = {Profile} />
+        </Rootdiv>
+      </React.Fragment>
+      
     );
   }
 }
