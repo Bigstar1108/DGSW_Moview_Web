59bd3f886d7c20c77fdae40f4ac37dc866745640
bigstar7966 <daesung09854@gmail.com>
2020-06-12 12:08:05 +0900

    [Add]
    setting store

--- moview/src/index.js
+++ moview/src/index.js
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Root from './Root';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import MoviewReducer from './modules/reducers/index';
+
+const store = createStore(MoviewReducer);
 
 ReactDOM.render(
-  <Root />,
+  <Provider store = {store}>
+    <Root />
+  </Provider>,
   document.getElementById('root')
 );
 
