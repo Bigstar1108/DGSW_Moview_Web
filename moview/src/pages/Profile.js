a70e437678688503bc982ea0cf798c468d13e950
bigstar7966 <daesung09854@gmail.com>
2020-06-12 12:07:50 +0900

    [Add]
    add profile screen

---
+++ moview/src/pages/Profile.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+import { connect } from 'react-redux';
+
+const Container = styled.div`
+    display : flex;
+    width : 100vw;
+    height : 100vh;
+`;
+
+class Profile extends React.Component{
+    state = {
+        member : JSON.parse(localStorage.getItem('member')),
+    }
+
+    render(){
+        return(
+            <Container>
+                <h1>Profile</h1>
+            </Container>
+        );
+    }
+}
+
+export default Profile;
