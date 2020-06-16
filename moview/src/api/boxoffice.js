201759540e48af50c24d4257a6a824fb288fa2c7
bigstar7966 <daesung09854@gmail.com>
2020-06-16 19:41:08 +0900

    [Del]

--- moview/src/api/boxoffice.js
+++
@@ -1,7 +0,0 @@
-import defaultApi from './defaultApi';
-
-const API_KEY = `key=${process.env.MOVIEW_API_KEY}`;
-
-export const getDaily = () => {
-    
-}
--- moview/src/api/defaultApi.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const defaultApi = axios.create({
-    baseURL : 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/',
-});
-
-export default defaultApi;
