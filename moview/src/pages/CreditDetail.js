140d2c35d5b2665c62b8dc2a0e3279e00e72d3ff
bigstar7966 <daesung09854@gmail.com>
2020-06-17 16:03:00 +0900

    [Add]
    add credit detail page

---
+++ moview/src/pages/CreditDetail.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class CreditDetail extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>credit</h1>
+            </div>
+        );
+    }
+}
+
+export default CreditDetail;
