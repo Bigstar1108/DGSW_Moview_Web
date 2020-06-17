a3d2cd172395d6e8c1b674e2f95913116b78ef9f
bigstar7966 <daesung09854@gmail.com>
2020-06-18 00:11:14 +0900

    [Fix]
    done

--- moview/src/components/CreditActorBox.js
+++ moview/src/components/CreditActorBox.js
@@ -4,16 +4,23 @@ import styled from 'styled-components';
 const Container = styled.div`
     display : flex;
     width : 20%;
-    height : 100%;
+    height : 60%;
     flex-direction : column;
-    background-color : blue;
+    margin-right : 3%;
 `;
 
 const ProfileImgBox = styled.div`
     display : flex;
     width : 100%;
-    height : 70%;
-    background-color : yellow;
+    height : 80%;
+    margin-bottom : 5px;
+`;
+
+const CreditText = styled.span`
+    font-family : 'Noto Sans KR', sans-serif;
+    font-size : 13px;
+    font-weight : bold;
+    color : black;
 `;
 
 class ActorBox extends React.Component{
@@ -21,12 +28,18 @@ class ActorBox extends React.Component{
         return(
             <Container>
                 <ProfileImgBox>
-                    <img
-                        style = {{width : "100%", height : "100%"}}
-                        alt = {`${this.props.character}역 ${this.props.name} 프로필 이미지`}
-                        src = {`https://image.tmdb.org/t/p/w200${this.props.poster}`}
-                    />
+                    {
+                        this.props.poster === null || this.props.poster === "" ? <span>이미지가 없습니다.</span>
+                        : <img
+                            style = {{width : "100%", height : "100%"}}
+                            alt = {`${this.props.character}역 ${this.props.name} 프로필 이미지`}
+                            src = {`https://image.tmdb.org/t/p/w200${this.props.poster}`}
+                            />
+                    }
                 </ProfileImgBox>
+
+                <CreditText>{this.props.character} 역</CreditText>
+                <CreditText>{this.props.name}</CreditText>
             </Container>
         );
     }
