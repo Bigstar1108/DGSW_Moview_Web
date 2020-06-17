c40f6bec0c67a93dedf23332ca63cdcda2e90f14
bigstar7966 <daesung09854@gmail.com>
2020-06-18 00:40:25 +0900

    [Fix]
    div set

--- moview/src/pages/CreditDetail.js
+++ moview/src/pages/CreditDetail.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
+import Header from '../components/Header';
+
+const Container = styled.div`
+    display : flex;
+    width : 100vw;
+    height : 100vh;
+    flex-direction : column;
+    align-items : center;
+`;
+
+const Body = styled.div`
+    display : flex;
+    width : 80%;
+    height : 100%;
+    padding-top : 80px;
+    flex-direction : column;
+`;
 
 class CreditDetail extends React.Component{
     render(){
         return(
-            <div>
-                <h1>credit</h1>
-            </div>
+            <Container>
+                <Header />
+
+                <Body>
+                    
+                </Body>
+            </Container>
         );
     }
 }
