d08e9c1df012c03923f5c003860d38e439f0c40e
bigstar7966 <daesung09854@gmail.com>
2020-06-21 00:57:37 +0900

    [Fix]
    add movie info and fix border style

--- moview/src/components/Search/MovieResultBox.js
+++ moview/src/components/Search/MovieResultBox.js
@@ -11,12 +11,8 @@ const Container = styled.div`
     flex-direction : row;
     align-items : center;
     border-top-width : 1.5px;
-    border-bottom-width : 1.5px;
-    border-bottom-color : black;
-    border-bottom-style : solid;
     border-top-color : black;
     border-top-style : solid;
-    margin-bottom : 5px;
 `;
 
 const ImageBox = styled.div`
@@ -25,6 +21,21 @@ const ImageBox = styled.div`
     height : 300px;
     justify-content : center;
     align-items : center;
+`;
+
+const InfoBox = styled.div`
+    display : flex;
+    width : 80%;
+    height : 100%;
+    flex-direction : column;
+`;
+
+const RateBox = styled.div`
+    display : flex;
+    width : 100%;
+    flex-direction : row;
+    align-items : center;
+    margin-left : 10px;
 `;
 
 class MovieResultBox extends React.Component{
@@ -41,6 +52,24 @@ class MovieResultBox extends React.Component{
                             /> : <span className = "adult">포스터 이미지가 없습니다.</span>
                         }
                     </ImageBox>
+
+                    <InfoBox>
+                        <span className = "title" style = {{marginTop : "5px"}}>{this.props.title}</span>
+                        {
+                            this.props.adult ? <span className = "adult">청소년 관람불가</span> : <span className = "adult">청소년 관람가능</span>
+                        }
+                        <span className = "date">{this.props.date}</span>
+                        <RateBox>
+                            <StarRatings
+                                rating = {this.props.vote_average}
+                                numberOfStars = {5}
+                                starRatedColor = "#F03535"
+                                starDimension = "15px"
+                                starSpacing = '3px'
+                            />
+                            <span className = "rating">{this.props.vote_average}</span>
+                        </RateBox>
+                    </InfoBox>
                 </Container>
             </Link>
         );
